c353f82aa8cb481633dd85b68d452ab09c5a22f9
[gyur1kim] <qri1114@gmail.com>
2022-03-05 23:52:16 +0900

    220305

--- 10.BrowserObjectModel/10.3_scroll.js
+++ 10.BrowserObjectModel/10.3_scroll.js
@@ -8,3 +8,10 @@ function stopScroll(){
 function autoScroll(){
     window.scrollBy(0, 10);
 }
+
+window.onbeforeprint = function(){
+    document.body.style.backgroundImage = "url('../images/10.4_logo.png')";
+}
+window.onafterprint = function(){
+    document.body.style.backgroundImage = "";
+}
