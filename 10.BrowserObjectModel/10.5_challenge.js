a9f2feafeed2d66062e24e7dba949ec96ce3d073
[gyur1kim] <qri1114@gmail.com>
2022-03-10 23:22:42 +0900

    220310

--- 10.BrowserObjectModel/10.5_challenge.js
+++ 10.BrowserObjectModel/10.5_challenge.js
@@ -1,2 +1,10 @@
-var div = document.querySelectorAll("#id>div");
-var x = window.screenX;
+function loading() {
+    var div = document.querySelectorAll("#snow>div");
+    var x = window.innerWidth;
+    div.forEach(function (i) {
+        i.style.position = "absolute";
+        i.style.display = "inline-block";
+        i.style.x = parseInt(Math.random() * x);
+        console.log(parseInt(Math.random() * x));
+    })
+}
