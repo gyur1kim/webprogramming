1a52042cc868227ab8b430085410a281efca4d60
[gyur1kim] <qri1114@gmail.com>
2022-03-12 23:52:01 +0900

    220312

--- 10.BrowserObjectModel/10.5_challenge.js
+++ 10.BrowserObjectModel/10.5_challenge.js
@@ -1,10 +1,37 @@
+var topValue = 0;
 function loading() {
-    var div = document.querySelectorAll("#snow>div");
+    // var div = document.querySelectorAll("#snow>div");
+    // var x = window.innerWidth;
+    // var y = window.innerHeight;
+    // div.forEach(function (item) {
+    //     console.log(item.id);
+    //     item.style.display = "inline-block";
+    //     item.style.position = "absolute";
+    //     item.style.left = parseInt(Math.random() * x) + "px";
+    // })
+    // while(topValue <= y){
+    //     div.forEach(function (item) {
+    //         setInterval(falling(item), 1000);
+    //     })
+    // }
+    var div = document.querySelector("#snow>div");
     var x = window.innerWidth;
-    div.forEach(function (i) {
-        i.style.position = "absolute";
-        i.style.display = "inline-block";
-        i.style.x = parseInt(Math.random() * x);
-        console.log(parseInt(Math.random() * x));
-    })
+    var y = window.innerHeight;
+    div.style.display = "inline-block";
+    div.style.position = "absolute";
+    div.style.left = parseInt(Math.random()*x) + "px";
+    while(topValue <= y){
+        console.log(topValue);
+        setTimeout(falling, 500);
+        if(topValue > y){
+            break;
+        }
+    }
+
+}
+
+function falling(){
+    var div = document.querySelector("#snow>div");
+    div.style.top = topValue + "px";
+    topValue += 10;
 }
