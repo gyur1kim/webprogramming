725113a07600509a9309b6c92ba4129734e1cca9
[gyur1kim] <qri1114@gmail.com>
2022-04-16 23:47:06 +0900

    220416

--- 13.오디오 비디오 위치정보 웹워커/13.3_webWorkers.js
+++ 13.오디오 비디오 위치정보 웹워커/13.3_webWorkers.js
@@ -7,3 +7,14 @@ for(let i = 0; i<10; i++){
 
 //메인 태스크와 워커 태스크는 오직 메시지를 이용해 통신한다.
 postMessage(sum);
+
+//반대로 메인 태스크에서 보낸 값을 알 수도 있음.
+onmessage = function (e){
+    let sum2 = 0;
+    let from = parseInt(e.data.from);
+    let to = parseInt(e.data.to);
+    for(let j=from; j<=to; j++){
+        sum2 += j;
+    }
+    postMessage(sum2);
+}
