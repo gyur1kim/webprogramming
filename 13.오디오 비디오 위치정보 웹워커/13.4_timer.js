39410646a8579454356c822e1584da623970b8c2
[gyur1kim] <qri1114@gmail.com>
2022-04-20 23:49:57 +0900

    220420

--- 13.오디오 비디오 위치정보 웹워커/13.4_timer.js
+++ 13.오디오 비디오 위치정보 웹워커/13.4_timer.js
@@ -1,19 +1,37 @@
-let time = 0;
 let timer = null;
-let count = 0;
+let sec = 0;
+let min = 0;
+let hour = 0;
+let time = "";
 
 onmessage = function(e){
     if(e.data === "start"){
-        console.log(++count);
+        if(timer != null){      //이 문장이 없으면 누를 때마다 setInterval이 생성된다.
+            return;
+        }
         timer = setInterval(myCallback, 1000);
+
     }
     else if(e.data === "stop"){
+        if(timer === null){
+            return;
+        }
         clearInterval(timer);
         close();
     }
 }
 
 function myCallback(){
-    time++;
+    time = "";
+    sec++;
+    if(sec >= 60){
+        min++;
+        sec = 0;
+    }
+    if(min >= 60){
+        hour++;
+        min = 0;
+    }
+    time += (hour<10?"0"+hour:hour) + ":" + (min<10?"0"+min:min)+":"+ (sec<10?"0"+sec:sec);
     postMessage(time);
 }
