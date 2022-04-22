655daa2d86ec8506b58fafec3793e3ce81a99680
[gyur1kim] <qri1114@gmail.com>
2022-04-22 23:46:25 +0900

    220422

--- 13.오디오 비디오 위치정보 웹워커/13.5_audioList.js
+++ 13.오디오 비디오 위치정보 웹워커/13.5_audioList.js
@@ -1,7 +1,30 @@
 let nowPlaying;
 let playBtn;
 let nextBtn;
+let playlist;
+let audioBar;
+let nowIdx;
 
 function onload(){
+     playlist = document.getElementById("playlist");
+     playBtn = document.getElementById("play");
+     nextBtn = document.getElementById("next");
+     audioBar = document.getElementById("audio")
 
+     playBtn.addEventListener("click", play);
+     nextBtn.addEventListener("click", next);
+     audioBar.addEventListener("ended", next);
+}
+
+function play(){
+    nowPlaying = playlist.options[playlist.selectedIndex].value;
+    audioBar.src = nowPlaying;
+    console.log(nowPlaying);
+}
+
+function next(){
+    nowIdx = playlist.selectedIndex;
+    nowIdx++;
+    nowPlaying = playlist.options[nowIdx].value;
+    console.log(nowPlaying);
 }
